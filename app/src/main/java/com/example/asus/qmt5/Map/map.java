4f0c6ae9d303de6f71f048cb0d1638ba7bb11d17
SCC <841779859@qq.com>
2018-05-28 11:37:58 +0800

    map

--- app/src/main/java/com/example/asus/qmt5/Map/map.java
+++ app/src/main/java/com/example/asus/qmt5/Map/map.java
@@ -64,7 +64,7 @@ public class map extends AppCompatActivity {
         mLocationClient.registerLocationListener(myListener);          //注册监听函数
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
-        SDKInitializer.initialize(getApplicationContext());
+        //SDKInitializer.initialize(getApplicationContext());
         mLocationClient.start();
         //setTitle(Map1Activity.this,com.example.cc.ditu2.R.layout.activity_map1);
         setContentView(R.layout.activity_map);
